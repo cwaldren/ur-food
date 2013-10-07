72bb9280e14c1629806b51986a98bcfd9f70467c
cwaldren <casey@fzy.me>
2013-10-06 20:24:44 -0400

    initial

---
+++ js/controllers.js
@@ -0,0 +1,125 @@
+
+
+function getTimeAndDay()
+{
+
+	var currentHour = moment().hour();
+	var day = moment().format("dddd");
+	var timeOfDay;
+	if (currentHour < 5) {
+		timeOfDay = " a very early " + day + " morning";
+	}
+	else if (currentHour < 7) {
+		timeOfDay = " an early " +  day + " morning";
+	}
+	else if (currentHour < 12) {
+		timeOfDay =  day + " morning";
+	}
+	else if (currentHour < 18) {
+		timeOfDay =  " a "+day + " afternoon";
+	}
+	else {
+		timeOfDay = " a "+day + " night";
+	}
+
+	return moment().format("h:mm") + " on " + timeOfDay;
+}
+function Location(name, close, simpleMenu)
+{
+	this.name = name;
+	this.close = close;
+	this.simpleMenu = simpleMenu;
+	this.equals = function(other) {
+     	return other.name == this.name;
+  };
+}
+function contains(arr, obj) {
+    var i = arr.length;
+    while (i--) {
+       if (arr[i].equals(obj)) {
+           return true;
+       }
+    }
+    return false;
+}
+
+function updateLocations()
+{
+	var openLocations = [];
+	var today = moment().format("dddd");
+	var inputDay;
+	if (today == "Monday" || today == "Tuesday" || today == "Wednesday" || today == "Thursday")
+	{
+		inputDay = "Monday-Thursday";
+	}
+	else
+	{
+		inputDay = today;
+	}
+	
+	for (var loc = 0; loc < dininghours.length; loc++)
+	{
+		//console.log(dininghours[loc].name)
+		//We need to check only the current day
+		
+		
+		for (var time = 0; time < dininghours[loc].days[inputDay].times.length; time++) 
+		{
+			
+			var theTime = dininghours[loc].days[inputDay].times[time];
+			var close = moment(theTime.close, "hhmma");
+			var open = moment(theTime.open, "hhmma");
+			console.log("timeslot: "+time+"location:"+dininghours[loc].name+" and thing:" + dininghours[loc].days[inputDay].times[time].what)
+			if (open.hour() > close.hour() && open.hour() != 12)
+			{
+				close.add('h', 24);
+			}
+			var range = moment().range(open, close);
+			//console.log("     open: "+open+"  close: "+close);
+			
+			if (range.contains(moment()))
+			{
+				//console.log(dininghours[loc].days[day])
+				var name = dininghours[loc].name;
+				var goodloc = new Location(name, theTime.close, theTime.what);
+				if (!contains(openLocations, goodloc))
+				{
+					openLocations.push(goodloc);
+					//console.log(loc)
+				}
+			}
+		
+		}
+			
+
+			
+		
+	}
+	
+	console.log(openLocations)
+	return openLocations;
+}
+
+function locationUpdater($scope, $timeout)
+{
+	$scope.timeAndDay = getTimeAndDay();
+	 $scope.locations = updateLocations();
+	// 	{name:"Danforth", close:"12pm", timeleft:"2 hours", simpleMenu:"brunch" },
+	// 	{name:"Hillside POD", close:"1pm", timeleft:"1 hours", simpleMenu:"groceries" },
+	// 	{name:"The Pit", close:"6pm", timeleft:"1 minute", simpleMenu:"the usual" }
+	// ];
+	
+	
+		
+	var timer = function(){
+		$scope.timeAndDay = getTimeAndDay();
+		$scope.locations = updateLocations();
+		$timeout(timer, 100);
+	} 
+	
+	timer();
+
+}
+
+var urHungryApp = angular.module('urHungryApp', []);
+urHungryApp.controller('locationUpdater', locationUpdater);
---
+++ js/hours.js
@@ -0,0 +1,266 @@
+var dininghours = 
+  [
+    {name: 'Danforth', days: {
+		
+    		'Monday-Thursday': {times:
+			[ 
+	    		{what: 'dinner'   , open: '5pm' , close: '9pm'},
+	    		{what: 'lunch'    , open: '11am', close: '1:30pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'lunch'    , open: '11am', close: '1:30pm'},
+	    		{what: 'select stations', open: '1:30pm', close: '5pm'},
+	    		{what: 'dinner'         , open: '5pm'   , close: '9pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'continental breakfast', open: '8am'    , close: '10:30am'},
+	    		{what: 'brunch'               , open: '10:30am', close: '2pm'},
+	    		{what: 'select stations'      , open: '2pm'    , close: '5pm'},
+	    		{what: 'dinner'               , open: '5pm'    , close: '9pm'},
+	    	]},	
+	    	'Sunday': {times: 
+			[
+	    		{what: 'continental breakfast', open: '8am'    , close: '10:30am'},
+	    		{what: 'brunch'               , open: '10:30am', close: '2pm'},
+	    		{what: 'select stations'      , open: '2pm'    , close: '5pm'},
+	    		{what: 'dinner'               , open: '5pm'    , close: '9pm'},
+	    	]}
+    
+	  	
+    	
+	}},
+
+	
+    {name: 'Hillside Market', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'groceries', open: '7:30am', close: '3am'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'groceries', open: '7:30am', close: '3am'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'groceries', open: '12pm', close: '3am'}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'groceries', open: '12pm', close: '3am'}
+	    	]}	
+    
+	    	
+    	
+	}},
+
+	 {name: 'Douglass', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'breakfast'      , open: '7:30am', close: '11am'},
+	    		{what: 'lunch'          , open: '11am'  , close: '2:30pm'},
+	    		{what: 'select stations', open: '2:30pm', close: '5pm'},
+	    		{what: 'dinner'         , open: '5pm'   , close: '8pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'breakfast', open: '7:30am', close: '11am'},
+				{what: 'lunch', open: '11am', close: '2pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]}	
+    
+	    	
+    	
+	}},
+
+	{name: 'The Pit', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'the usual', open: '8:30am', close: '11:59pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'the usual', open: '8:30am', close: '11:59pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'the usual', open: '11am', close: '11:59pm'}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'the usual', open: '11am', close: '11:59pm'}
+	    	]}	
+   	}},
+
+   		{name: 'Blimpie', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'subs', open: '11am', close: '10pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'subs', open: '11am', close: '10pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'subs', open: '11am', close: '10pm'}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'subs', open: '11am', close: '10pm'}
+	    	]}	
+   		}},
+
+   		{name: 'Starbucks', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'coffee', open: '7:30am', close: '11:59pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'coffee', open: '7:30am', close: '1am'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'coffee', open: '10am', close: '1am'}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'coffee', open: '10am', close: '11:59pm'}
+	    	]}	
+   		}},
+
+   		{name: 'The Mel Express', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'lunch', open: '11am', close: '2pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'lunch', open: '11am', close: '2pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]}	
+   		}},
+
+   		{name: 'Grab & Go', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'bags of food', open: '10:30am', close: '2pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'bags of food', open: '10:30am', close: '2pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]}	
+   		}},
+
+   		{name: 'Faculty Club', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'lunch', open: '11am', close: '2pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'lunch', open: '11am', close: '2pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]}	
+   		}},
+
+   		{name: 'Pura Vida', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'lunch', open: '7:30am', close: '6:30pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'lunch', open: '11am', close: '2pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]}	
+   		}},
+
+   		{name: 'Connections', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'café', open: '7am', close: '10pm'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'café', open: '7am', close: '5pm'}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'closed', open: null, close: null}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'café', open: '2pm', close: '10pm'}
+	    	]}	
+   		}},
+
+   		{name: 'Southside Market', days: {
+		
+    		'Monday-Thursday' : {times:
+			[ 
+	    		{what: 'groceries', open: '5pm', close: '1am'}
+	    	]},
+	    	'Friday': {times: 
+			[ 
+				{what: 'closed', open: null, close: null}
+	    	]},		
+	    	'Saturday': {times:
+			[
+	    		{what: 'groceries', open: '12pm', close: '5pm'}
+	    	]},	
+	    	'Sunday': {times:
+			[
+	    		{what: 'groceries', open: '5pm', close: '1am'}
+	    	]}	
+   		}},
+
+];
